651feb8a34f5d023dab9e6549d1edda3e42290c3
Arham-94 <arhamkhanch94@gmail.com>
2025-06-17 17:02:43 +0500

    Update database again in settings.py

--- portfolioProject/settings.py
+++ portfolioProject/settings.py
@@ -85,39 +85,39 @@ DB_LIVE =os.environ('DB_LIVE')
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# if DB_LIVE in ["False", False]:
+if DB_LIVE in ["False", False]:
 
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-
-# else:
-
-
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": os.environ('DB_NAME'),
-#             "USER": os.environ('DB_USER'),
-#             "PASSWORD": os.environ('DB_PASSWORD'),
-#             "HOST": os.environ('DB_HOST'),
-#             "PORT": os.environ('DB_PORT'),
-#         }
-#     }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ('DB_NAME'),
-        "USER": os.environ('DB_USER'),
-        "PASSWORD": os.environ('DB_PASSWORD'),
-        "HOST": os.environ('DB_HOST'),
-        "PORT": os.environ('DB_PORT'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ('DB_NAME'),
+            "USER": os.environ('DB_USER'),
+            "PASSWORD": os.environ('DB_PASSWORD'),
+            "HOST": os.environ('DB_HOST'),
+            "PORT": os.environ('DB_PORT'),
+        }
+    }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ('DB_NAME'),
+#         "USER": os.environ('DB_USER'),
+#         "PASSWORD": os.environ('DB_PASSWORD'),
+#         "HOST": os.environ('DB_HOST'),
+#         "PORT": os.environ('DB_PORT'),
+#     }
+# }
 
 
 
